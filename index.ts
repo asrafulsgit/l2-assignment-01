4893d8cee9f10b20d67505bb731c572cfbb51b78
sourob2356 <sourob2356@gmail.com>
2025-05-07 22:31:28 +0600

    assignment tasks completed

--- index.ts
+++ index.ts
@@ -1,1 +1,114 @@
-console.log('hello')
+function formatString(input: string, toUpper?: boolean): string{
+
+    if( toUpper  === true || toUpper === undefined){
+        return input.toUpperCase()
+    }
+
+    return input.toLowerCase()
+}
+// console.log(formatString("Hello", false))
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    const filter = items.filter(item => item.rating >= 4 )
+    return filter
+}
+
+// console.log(filterByRating())
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    return arrays.reduce((array, curr) => [...array, ...curr], []);
+}
+// console.log(concatenateArrays([1, 2,2], [3, 4], [5]))
+
+class Vehicle {
+    private make : string;
+    private year : number;
+    constructor(make : string,year : number){
+        this.make = make;
+        this.year = year;
+    }
+
+    getInfo(){
+        console.log(`Make: ${this.make}, Year: ${this.year}`)
+    }
+}
+
+class Car extends Vehicle{
+    private model : string;
+    constructor(make : string,year : number,model :string){
+        super(make,year)
+        this.model = model;
+    }
+
+    getModel(){
+        console.log(`Model : ${this.model}`)
+    }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+// myCar.getInfo()
+// myCar.getModel()
+
+function processValue(value: string | number): number{
+   
+    if(typeof value === 'string'){
+        return value.length;
+    }
+
+    return value * 2;
+}
+// console.log(processValue(10));
+
+interface Product {
+    name: string;
+    price: number;
+  }
+  
+function getMostExpensiveProduct(products: Product[]): Product | null{
+
+    if(products.length === 0) return null;
+
+    return products.reduce((highest,curr)=>{
+        return curr.price > highest.price ? curr : highest;
+    })
+
+}
+// const products = [
+//     { name: "Pen", price: 80 },
+//     { name: "Notebook", price: 25 },
+//     { name: "Bag", price: 50 }
+//   ];
+// console.log(getMostExpensiveProduct(products));
+
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  function getDayType(day: Day): string{
+    if(day === Day.Friday || day === Day.Saturday){
+        return "Weekend"
+    }
+    return "Weekday"
+  }
+// console.log(getDayType(Day.Friday))
+
+async function squareAsync(n: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (n < 0) {
+                reject(new Error("Negative number not allowed"));
+            } else {
+                resolve(n * n);
+            }
+        }, 1000);
+    });
+}
+
+// squareAsync(4).then(console.log);        
+// squareAsync(-3).catch(console.error);   
---
+++ main/index.js
@@ -0,0 +1,99 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function formatString(input, toUpper) {
+    if (toUpper === true || toUpper === undefined) {
+        return input.toUpperCase();
+    }
+    return input.toLowerCase();
+}
+// console.log(formatString("Hello", false))
+function filterByRating(items) {
+    const filter = items.filter(item => item.rating >= 4);
+    return filter;
+}
+// console.log(filterByRating())
+function concatenateArrays(...arrays) {
+    return arrays.reduce((array, curr) => [...array, ...curr], []);
+}
+// console.log(concatenateArrays([1, 2,2], [3, 4], [5]))
+class Vehicle {
+    constructor(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    getInfo() {
+        console.log(`Make: ${this.make}, Year: ${this.year}`);
+    }
+}
+class Car extends Vehicle {
+    constructor(make, year, model) {
+        super(make, year);
+        this.model = model;
+    }
+    getModel() {
+        console.log(`Model : ${this.model}`);
+    }
+}
+const myCar = new Car("Toyota", 2020, "Corolla");
+// myCar.getInfo()
+// myCar.getModel()
+function processValue(value) {
+    if (typeof value === 'string') {
+        return value.length;
+    }
+    return value * 2;
+}
+function getMostExpensiveProduct(products) {
+    if (products.length === 0)
+        return null;
+    return products.reduce((highest, curr) => {
+        return curr.price > highest.price ? curr : highest;
+    });
+}
+// const products = [
+//     { name: "Pen", price: 80 },
+//     { name: "Notebook", price: 25 },
+//     { name: "Bag", price: 50 }
+//   ];
+// console.log(getMostExpensiveProduct(products));
+var Day;
+(function (Day) {
+    Day[Day["Monday"] = 0] = "Monday";
+    Day[Day["Tuesday"] = 1] = "Tuesday";
+    Day[Day["Wednesday"] = 2] = "Wednesday";
+    Day[Day["Thursday"] = 3] = "Thursday";
+    Day[Day["Friday"] = 4] = "Friday";
+    Day[Day["Saturday"] = 5] = "Saturday";
+    Day[Day["Sunday"] = 6] = "Sunday";
+})(Day || (Day = {}));
+function getDayType(day) {
+    if (day === Day.Friday || day === Day.Saturday) {
+        return "Weekend";
+    }
+    return "Weekday";
+}
+// console.log(getDayType(Day.Friday))
+function squareAsync(n) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (n < 0) {
+                    reject(new Error("Negative number not allowed"));
+                }
+                else {
+                    resolve(n * n);
+                }
+            }, 1000);
+        });
+    });
+}
+// squareAsync(4).then(console.log);        
+// squareAsync(-3).catch(console.error);   
